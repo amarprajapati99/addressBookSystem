7d3a0c2ba06df6f78abc47b48511fa3253a091e9
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-26 14:51:34 +0530

    Add the details of address book system

--- src/com/addressBookSystem/AddressBookSystem.java
+++ src/com/addressBookSystem/AddressBookSystem.java
@@ -1,9 +1,46 @@
 package com.addressBookSystem;
 
-public class AddressBookSystem {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class AddressBookSystem {
+	public void addressBook() {
+
 		System.out.println("Welcome to address book program");
 
+		String name, lastName, address, city, state, emailId;
+		int zipCode, mobileNumber;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your first name");
+		name = sc.nextLine();
+
+		System.out.println("Enter your last name");
+		lastName = sc.nextLine();
+
+		System.out.println("Enter your address");
+		address = sc.nextLine();
+
+		System.out.println("Enter your city");
+		city = sc.nextLine();
+
+		System.out.println("Enter your state");
+		sc.nextLine();
+
+		System.out.println("Enter your email id");
+		emailId = sc.nextLine();
+		System.out.println("Enter your zip code");
+		zipCode = sc.nextInt();
+
+		System.out.println("Enter your mobile nuber");
+		mobileNumber = sc.nextInt();
+
+		System.out.println(name + "" + lastName);
+		System.out.println(address + " " + city + " " + zipCode + " ," + mobileNumber + " " + emailId);
+
+	}
+
+	public static void main(String[] args) {
+		AddressBookSystem addressObject = new AddressBookSystem();
+		addressObject.addressBook();
 	}
 }
